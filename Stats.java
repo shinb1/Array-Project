d074fd8ca232374dbf14b7740c787de68cb793c6
apcs <apcs@mvhs-imac31u528.dcsdk12.org>
2013-09-19 09:32:24 -0600

    finished max-quartile 3

---
+++ Stats.java
@@ -0,0 +1,149 @@
+public class Stats {
+
+	public static void main(String[] args) {
+		int[] a = {9, 12, 3, 4, 15, 19, 26};
+		
+
+		System.out.println(quartile1(a));
+		// print(organize(a));
+	}
+
+	
+
+	public static int max(int[] a) {
+		
+		int x = a[0];
+
+		for (int i=1; i < a.length; i++) {
+			
+			if (a[i]>x) {
+				x = a[i];
+			}	
+
+		}
+
+		return x;
+
+	}
+
+	public static int min(int[] a) {
+
+		int x = a[0];
+
+		for (int i=1; i < a.length; i++) {
+			
+			if (a[i]<x) {
+				x = a[i];
+			}	
+
+		}
+
+		return x;
+
+	}
+
+	public static double mean(int[] a) {
+
+		double x = 0.0;
+
+		for (int i=0; i < a.length; i++) {
+			x = x + a[i];
+		}
+
+		x = x/a.length;
+  
+		return x;
+	}
+
+	public static double median(int[] a) {
+
+		double middle = 0;
+		organize(a);
+
+		if (a.length % 2 == 1) {
+			middle = a[a.length/2];
+		} else {
+			middle = ((a[a.length/2] + a[a.length/2 + 1])/2);
+		}
+
+		System.out.println("The median is: ");
+		return middle;
+
+	}
+
+	public static int quartile1(int[] a) {
+
+		int q1 = 0;
+		organize(a);
+
+		q1 = a[((a.length+1)/4)-1];
+
+		System.out.println("The number indicating the first quartile is: ");
+		return q1;
+
+	}
+
+	public static int quartile3(int[] a) {
+
+		int q3 = 0;
+		organize(a);
+
+		q3 = a[((a.length+1)/4*3)-1];
+
+		System.out.println("The number indicating the third quartile is: ");
+		return q3;
+
+	}
+
+	public static int[] organize(int[] a) {
+
+		int x=0;
+		int y=0;
+
+		for (int i=0; i<a.length; i++) {
+			for (int j=0; j<a.length-1; j++) {
+				if (a[j] > a[j+1]) {
+
+					x = a[j];
+					y = a[j+1];
+					a[j] = y;
+					a[j+1] = x;
+
+				}
+			}
+		}
+
+		return a;
+
+	}
+
+	public static int print(int[] a) {
+
+		for (int i=0; i<a.length; i++) {
+			System.out.println(a[i]);
+		}
+
+		return 0;
+
+	}
+
+}
+
+
+
+
+// max
+
+// min
+
+// mean
+
+// median
+
+// quartile 1
+
+//quartile 3
+
+// mode
+
+// standard deviation
